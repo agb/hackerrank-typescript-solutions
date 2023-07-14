03746d817bdd37ca852dbda8ca33f4eac675b371
Ali Bektash <agokselb@gmail.com>
2023-07-14 19:15:31 +0900

    Create week-1-divisible-sum-pairs.ts

---
+++ week-1-divisible-sum-pairs.ts
@@ -0,0 +1,65 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+function divisibleSumPairs(n: number, k: number, ar: number[]): number {
+    let howMany = 0;
+    let cloneArray = ar
+
+    while(cloneArray.length > 1) {
+        howMany += cloneArray.filter((a,b,arr) =>  (b>0)&&(arr[0]+arr[b])%k===0 ).length;
+
+        cloneArray.shift()
+    }
+
+    return howMany;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n: number = parseInt(firstMultipleInput[0], 10);
+
+    const k: number = parseInt(firstMultipleInput[1], 10);
+
+    const ar: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result: number = divisibleSumPairs(n, k, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
