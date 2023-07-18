43aafbf93496adf2ebf20617cb4cd74f8ae55702
Ali Bektash <agokselb@gmail.com>
2023-07-18 19:43:41 +0900

    Create week-2-counting-sort-1.ts

---
+++ week-2-counting-sort-1.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr: number[]): number[] {
+    const isBigOne = (arr.length > 100)
+    let countingArr:number[] = []
+    
+    arr.forEach((value,index) => {
+        
+        if(!isBigOne && !countingArr[index]) countingArr[index] = 0;
+
+        countingArr[value] = (!countingArr[value]) ? 1 : countingArr[value]+1
+    })
+
+    return countingArr;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result: number[] = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
