efdb5079e2156217a06458a78d96924c64c6449c
Ali Bektash <agokselb@gmail.com>
2023-08-06 18:01:38 +0900

    Create week-3-migratory-birds.ts

---
+++ week-3-migratory-birds.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr: number[]): number {
+    const typesLength:number[] = [0,0,0,0,0,0]
+    let max = 0;
+       
+    for(let birdType of arr) {
+        typesLength[birdType]++;
+        const current = typesLength[birdType];
+                
+        if(current > max) max = current;
+    }
+
+    return typesLength.findIndex((value) => value === max)
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const arrCount: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result: number = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
