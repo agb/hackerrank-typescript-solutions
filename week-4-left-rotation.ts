7e208f911a0f2cb9a64f419aaa41a9f15497be06
Ali Bektash <agokselb@gmail.com>
2023-08-15 15:50:01 +0900

    Create week-4-left-rotation.ts

---
+++ week-4-left-rotation.ts
@@ -0,0 +1,65 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function rotateLeft(d: number, arr: number[]): number[] {
+   let count = 1;
+   
+   do {
+       const current = arr[0]; 
+       
+       arr.shift();
+       count++;
+       arr.push(current)
+   } while(count <= d) 
+   
+    return arr;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n: number = parseInt(firstMultipleInput[0], 10);
+
+    const d: number = parseInt(firstMultipleInput[1], 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result: number[] = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
