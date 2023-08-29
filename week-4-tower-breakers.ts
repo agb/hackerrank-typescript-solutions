fdba1889f39ae2c271b92f19e2a5dfbc84d59b46
Ali Bektash <agokselb@gmail.com>
2023-08-29 17:25:05 +0900

    Create week-4-tower-breakers.ts

---
+++ week-4-tower-breakers.ts
@@ -0,0 +1,63 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'towerBreakers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ */
+
+function towerBreakers(n: number, m: number): number {
+        
+    const towers = Array(n).fill(m);
+  
+    if(m === 1 || towers.length % 2 === 0) 
+        return 2;
+
+    return 1;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const t: number = parseInt(readLine().trim(), 10);
+
+    for (let tItr: number = 0; tItr < t; tItr++) {
+        const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n: number = parseInt(firstMultipleInput[0], 10);
+
+        const m: number = parseInt(firstMultipleInput[1], 10);
+
+        const result: number = towerBreakers(n, m);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
